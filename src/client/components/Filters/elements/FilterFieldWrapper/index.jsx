4130dfd4b69c9cd800fc0a8853ab237979fa85cf
Ian Leggett <contactleggett@gmail.com>
2020-10-05 11:42:33 +0100

    Add adviser typeahead filter

---
+++ src/client/components/Filters/elements/FilterFieldWrapper/index.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+import { FieldWrapper } from '../../../../components'
+
+const FilterFieldWrapper = styled(FieldWrapper)`
+  label {
+    font-weight: normal;
+  }
+`
+
+export default FilterFieldWrapper
